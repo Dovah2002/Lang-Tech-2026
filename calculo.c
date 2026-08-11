69571f3a749da93cb5695e1350e05ad86a3d67c0
Dovah2002 <143543628+Dovah2002@users.noreply.github.com>
2026-08-11 11:41:10 -0300

    Implement basic calculation in calculo.c

---
+++ calculo.c
@@ -0,0 +1,19 @@
+# include <stdio.h>
+# include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+
+int a, b, c, r1, r2;
+
+a = 5;
+b = 11;
+c = 85;
+r1 = a+b+c;
+r2 = r1/3;
+
+printf("%i + %i + %i => %i/3 = %i", a,b,c,r1,r2);
+
+return 0:
+
+
+}
