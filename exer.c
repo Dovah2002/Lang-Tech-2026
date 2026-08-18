94940a6b13222125754344cac8367d04f75998a2
Dovah2002 <143543628+Dovah2002@users.noreply.github.com>
2026-08-17 22:27:13 -0300

    Implement input for ordered pairs in exer.c
    Added code to read ordered pairs (p1 and p2) from user input.

---
+++ exer.c
@@ -0,0 +1,51 @@
+#include <iostream>
+
+int main(int argc, char** argv) {
+/*	
+  int n, res;
+  int bit_64, bit_32, bit_16, bit_8, bit_4, bit_2;
+  printf("Insira o valor: ");
+  scanf("%d", &n);
+  
+  bit_64 = n%2;
+  res = n/2;
+  
+  bit_32 = res%2;
+  res = res/2;
+  
+  bit_16 = res%2;
+  res = res/2;
+  
+  bit_8 = res%2;
+  res = res/2;
+  
+  bit_4 = res%2;
+  res = res/2;
+  
+  bit_2 = res%2;
+  res = res/2;
+  
+  printf("O numero %d em bin: %d%d%d%d%d%d%d", n, res%2, bit_2, bit_4, bit_8, bit_16, bit_32, bit_64);
+  
+  */
+  
+  int x1, x2, y1, y2, p1, p2;
+  float dis;
+  
+  printf("insira o valor do par ordenado p1(x1,y1)")
+  scanf("%d",&x1);
+  scanf("%d",&y1);
+  
+   printf("insira o valor do par ordenado p2(x2,y2)")
+  scanf("%d",&x2);
+  scanf("%d",&y2);
+  
+  
+  
+  printf("\n\")
+  
+  
+  
+ 	
+	return 0;
+}
